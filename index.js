f769cca29cc872bba507c714d0169d5dda658831
Saki <sbasken@gmail.com>
2023-02-08 17:28:27 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer;
+const leastFavoriteCustomer = "you"
+
+
+function upperCaseCustomerName(e) {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'me'
+}
